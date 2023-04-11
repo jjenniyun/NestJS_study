cdf5a6a125ccdb5713759f3c8fe7afd7128ba1c5
jjenniyun <hjkjj1847@gmail.com>
2023-04-11 17:02:00 +0900

    express오류수정(23.04.11)

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -1,3 +1,13 @@
 "use strict";
-console.log("hello world!!");
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+var port = 8000;
+app.get("/test", function (req, res) {
+    console.log(req);
+    res.send({ name: "hwang min hyun", age: 29, friends: ["ss", "yy", "us"] });
+});
+app.listen(port, function () {
+    console.log("Example app listening at http://localhost:" + port);
+});
 //# sourceMappingURL=app.js.map
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,1 +1,13 @@
-console.log("hello world!!");
+import * as express from "express";
+
+const app: express.Express = express();
+const port: number = 8000;
+
+app.get("/test", (req: express.Request, res: express.Response) => {
+  console.log(req);
+  res.send({ name: "hwang min hyun", age: 29, friends: ["ss", "yy", "us"] });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
